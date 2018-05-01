940642de132e908d317b9067cab450f1796a7d9d
justinpolchinski <justinpolchinski@gmail.com>
2018-05-01 13:48:19 -0400

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //Dependencies
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
@@ -9,7 +10,9 @@ var PORT = process.env.PORT || 3001;
 // for data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.get('/', function (req, res) {
+  res.send('Hello Node + GitHub! This code push has auto-deployed!');
+});
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
