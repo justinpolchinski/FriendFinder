ceaa0152fda416a12dc1a32032f0ba1b4c12a1f8
justinpolchinski <justinpolchinski@gmail.com>
2018-05-01 13:59:48 -0400

    removed get

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ var PORT = process.env.PORT || 3001;
 // for data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get('/', function (req, res) {
-  res.send('Hello Node + GitHub! This code push has auto-deployed!');
-});
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
